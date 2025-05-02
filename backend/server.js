b8d5335f467b27c2652bb97fc7ef2b0e3658948a
manthanpendke <139007548+manthanpendke@users.noreply.github.com>
2025-05-03 01:36:10 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -11,9 +11,9 @@ app.use(bodyParser.json());
 app.use('/api/tasks', tasksRoutes);
 
 // Serve frontend build
-app.use(express.static(path.join(__dirname, '../frontend/build')));
+app.use(express.static(path.join(__dirname, '../frontend/dist')));
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../frontend/build/index.html'));
+  res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
 });
 
 const PORT = process.env.PORT || 5000;
